549b50707a86360a167155b6b931a63420d5ead0
David Chelimsky <dchelimsky@gmail.com>
2019-05-16 16:17:30 -0500

    use ListObjectsV2 in s3 examples

--- examples/s3_examples.clj
+++ examples/s3_examples.clj
@@ -17,7 +17,8 @@
 ;; what can it do?
 (aws/ops s3)
 ;; op names
-(-> (aws/ops s3) keys)
+(-> (aws/ops s3) keys sort)
+
 ;; op doc-data
 (-> (aws/ops s3) :CreateBucket)
 ;; a little more friendly
@@ -51,7 +52,7 @@
 (aws/invoke s3 {:op :ListBuckets})
 
 ;; no objects yet ...
-(aws/invoke s3 {:op :ListObjects :request {:Bucket bucket-name}})
+(aws/invoke s3 {:op :ListObjectsV2 :request {:Bucket bucket-name}})
 
 ;; Body is blob type, for which we accept a byte-array or an InputStream
 (aws/invoke s3 {:op :PutObject :request {:Bucket bucket-name :Key "hello.txt"
@@ -61,7 +62,7 @@
                                          :Body (io/input-stream (.getBytes "Oh hai!"))}})
 
 ;; now you should see the object you just added
-(aws/invoke s3 {:op :ListObjects :request {:Bucket bucket-name}})
+(aws/invoke s3 {:op :ListObjectsV2 :request {:Bucket bucket-name}})
 
 ;; Body is a blob type, which always returns an InputStream
 (aws/invoke s3 {:op :GetObject :request {:Bucket bucket-name :Key "hello.txt"}})
@@ -74,7 +75,7 @@
 (aws/invoke s3 {:op :DeleteObjects, :request {:Delete {:Objects [{:Key "hello.txt"}]}, :Bucket bucket-name}})
 
 ;; poof, the object is gone!
-(aws/invoke s3 {:op :ListObjects :request {:Bucket bucket-name}})
+(aws/invoke s3 {:op :ListObjectsV2 :request {:Bucket bucket-name}})
 
 (aws/invoke s3 {:op :DeleteBucket :request {:Bucket bucket-name}})
 
