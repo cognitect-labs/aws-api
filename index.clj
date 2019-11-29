3b92daf9fded0ae0f6a01c04f50473138a9762c2
David Chelimsky <dchelimsky@gmail.com>
2019-11-29 15:11:08 -0600

    Updating gh-pages with new api docs

--- index.clj
+++ index.clj
@@ -9,10 +9,6 @@
    :name "cognitect.aws.credentials",
    :wiki-url "cognitect.aws.credentials-api.html",
    :source-url nil}
-  {:doc nil,
-   :name "cognitect.aws.dynaload",
-   :wiki-url "cognitect.aws.dynaload-api.html",
-   :source-url nil}
   {:doc
    "Region providers. Primarily for internal use, and subject to change.",
    :name "cognitect.aws.region",
