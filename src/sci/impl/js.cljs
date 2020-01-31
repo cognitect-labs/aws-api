986ba1ce6676e1760f62b991787c37d2f5a0abbc
Jeroen van Dijk <jeroentjevandijk@gmail.com>
2020-01-31 15:26:38 +0100

    Steal Sci’s compile script for a CLJS library

---
+++ src/sci/impl/js.cljs
@@ -0,0 +1,59 @@
+(ns sci.impl.js
+  "JavaScript interface to sci."
+  {:no-doc true}
+  (:require [cognitect.aws.client]))
+
+(defn map-vals [f m]
+  (reduce-kv (fn [m k v] (assoc m k (f v))) {} m))
+
+(defn map-keys [f m]
+  (reduce-kv (fn [m k v] (assoc m (f k) v)) {} m))
+
+(defn symbolize-keys [m]
+  (map-keys symbol m))
+
+(defn- js-opts->cljs-opts [opts]
+  (let [cljs (js->clj opts)
+        bindings (get cljs "bindings")
+        bindings (symbolize-keys bindings)
+        namespaces (get cljs "namespaces")
+        namespaces (map-vals symbolize-keys namespaces)
+        namespaces (symbolize-keys namespaces)
+        allow (get cljs "allow")
+        allow (mapv symbol allow)
+        deny (get cljs "deny")
+        deny (mapv symbol deny)
+        realize-max (get cljs "realizeMax")
+        preset (when-let [v (get cljs "preset")]
+                 (keyword v))
+        classes (get cljs "classes")
+        allow-all-classes (get classes "allow")
+        classes (symbolize-keys classes)
+        classes (if allow-all-classes (assoc classes :allow allow-all-classes) classes)
+        env (get cljs "env")]
+    {:bindings bindings
+     :namespaces namespaces
+     :allow allow
+     :deny deny
+     :preset preset
+     :realize-max realize-max
+     :classes classes
+     :env env}))
+
+(defn ^:export toJS [v]
+  (if (instance? MetaFn v)
+    ;; when returning a function, make it callable from JS
+    (.-afn v)
+    (clj->js v)))
+
+(defn ^:export evalString
+  "Evaluates string `s` as a Clojure form using the Small Clojure Interpreter.
+
+  The object `opts` may contain the following:
+
+  - `\"bindings\"`: an object with bindings that are used to resolve symbols
+  in the Clojure form, e.g. `{'x 1}`."
+  ([s] (evalString s nil))
+  ([s opts]
+  #_ (sci/eval-string s (js-opts->cljs-opts opts))))
+
