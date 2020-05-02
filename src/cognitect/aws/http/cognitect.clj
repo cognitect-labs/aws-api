ace87a0e0ea1653c95c1f08305785b287b661d27
Ghadi Shayban <ghadi@cognitect.com>
2020-05-01 20:59:11 -0400

    changes to http-client

--- src/cognitect/aws/http/cognitect.clj
+++ src/cognitect/aws/http/cognitect.clj
@@ -13,6 +13,10 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
+;; copied and modified from cognitect.http-client
+
+;; TODO reconsider pending ops check, when there are lazy request/response bodies
+
 (ns ^:skip-wiki cognitect.aws.http.cognitect
   (:require
    [cognitect.aws.http :as aws]
@@ -23,11 +27,12 @@
    [java.nio ByteBuffer]
    [java.util.concurrent RejectedExecutionException TimeUnit TimeoutException]
    [org.eclipse.jetty.client HttpClient Socks4Proxy]
+   [org.eclipse.jetty.http HttpField]
    [org.eclipse.jetty.client.api Request Response Result
                                  Response$CompleteListener Response$HeadersListener Response$ContentListener]
    [org.eclipse.jetty.client.util ByteBufferContentProvider]
    [org.eclipse.jetty.util.resource Resource]
-   org.eclipse.jetty.util.ssl.SslContextFactory))
+   [org.eclipse.jetty.util.ssl SslContextFactory SslContextFactory$Client]))
 
 (set! *warn-on-reflection* true)
 
@@ -110,36 +115,17 @@
       req)))
 
 (defn- on-headers
-  "Helper for submit. Adds :status, :headers, and :body to state based on
+  "Helper for submit. Adds :status and :headers to state based on
    response."
   [state ^Response response]
-  (let [jetty-headers (.getHeaders response)
-        headers (reduce
-                  (fn [m ^String n]
-                    (assoc m
-                      (.toLowerCase n java.util.Locale/ENGLISH)
-                      (.getValue (.getField jetty-headers n))))
-                  {}
-                  (.getFieldNamesCollection jetty-headers))]
-    (cond-> (assoc state
-              :status (.getStatus response)
-              :headers headers)
-      (response-body? (.. response getRequest getMethod))
-      (assoc :body (let [lstr (get headers "content-length")
-                         ln (try
-                              (Long/parseLong lstr)
-                              (catch NumberFormatException _ nil))]
-                     (when (and ln (not (zero? ln)))
-                       (empty-bbuf ln)))))))
-
-;; jetty requires that we copy the buffer out before returning!
-;; See https://www.eclipse.org/jetty/javadoc/9.4.7.v20170914/org/eclipse/jetty/client/api/Response.ContentListener.html#onContent-org.eclipse.jetty.client.api.Response-java.nio.ByteBuffer-
-(defn- on-content
-  "Helper for submit. Appends content to state :body"
-  [{:keys [body] :as state} ^ByteBuffer content]
-  (update state :body #(append-buffer
-                        (or % (empty-bbuf (.remaining content)))
-                        content)))
+  (let [headers (-> (reduce (fn [m ^HttpField f]
+                              (assoc! m (.getLowerCaseName f) (.getValue f)))
+                            (transient {})
+                            (.getHeaders response))
+                    persistent!)]
+    (assoc state
+           :status (.getStatus response)
+           :headers headers)))
 
 (defn error->category
   "Guess what categoric thing went wrong based on jetty exception.
@@ -251,24 +237,7 @@ On error, response map is per cognitect.anomalies"
     ch)
   (-stop [_] (.stop jetty-client)))
 
-(defn ssl-context-factory
-  ^SslContextFactory [{:keys [trust-all classpath-trust-store trust-store-password trust-store validate-hostnames]
-                       :or {validate-hostnames true}}]
-  (let [factory (SslContextFactory. false)] ;;(boolean trust-all)
-    (if validate-hostnames
-      (.setEndpointIdentificationAlgorithm factory "HTTPS")
-      ;; Default changed in 9.4.15
-      ;; https://github.com/eclipse/jetty.project/issues/3466
-      (.setEndpointIdentificationAlgorithm factory nil))
-    (when trust-store
-      (.setTrustStore factory trust-store))
-    (when classpath-trust-store
-      (.setTrustStoreResource factory (Resource/newClassPathResource classpath-trust-store)))
-    (when trust-store-password
-      (.setTrustStorePassword factory trust-store-password))
-    factory))
-
-(defn create*
+(defn create
   "Creates an http-client that can be used with submit. Takes a config map with
    the following keys:
 
@@ -276,46 +245,41 @@ On error, response map is per cognitect.anomalies"
    :connect-timeout                  in msec, default 5000
    :max-connections-per-destination  default 64
    :pending-ops-limit                default 64
-   :classpath-trust-store            classpath location of trust store
-   :trust-store-password             trust store password
-   :trust-store                      java.security.KeyStore instance
-   :validate-hostnames               boolean, defaults to true
-   :proxy-host                       optional host to use for proxy, string, defaults to 'localhost' if proxy-port is provided with no proxy-host
-   :proxy-port                       optional port to use for proxy, int"
-  [{:keys [resolve-timeout connect-timeout max-connections-per-destination
-           pending-ops-limit proxy-host proxy-port]
-    :or   {resolve-timeout 5000
-           connect-timeout 5000
-           max-connections-per-destination 64
-           pending-ops-limit 64}
-    :as   config}]
-  (let [jetty-client (doto (HttpClient. (ssl-context-factory config))
-                       (.setAddressResolutionTimeout resolve-timeout)
-                       (.setConnectTimeout connect-timeout)
-                       (.setMaxConnectionsPerDestination max-connections-per-destination)
-                       ;; these are Jetty defaults, except for the queue size
-                       (.setExecutor (doto (org.eclipse.jetty.util.thread.QueuedThreadPool.
-                                            200 8 60000
-                                            (java.util.concurrent.LinkedBlockingQueue. ^int (* 2 pending-ops-limit)))
-                                       (.setDaemon true))))]
-    ;; This is jetty defaults, except daemonizing scheduler
-    (.setScheduler jetty-client (org.eclipse.jetty.util.thread.ScheduledExecutorScheduler.
+   :min-threads                      default 4
+   :max-threads                      default 50"
+  ([] (create {}))
+  ([{:keys [resolve-timeout connect-timeout max-connections-per-destination
+            pending-ops-limit max-threads min-threads]
+      :or   {resolve-timeout 5000
+             connect-timeout 5000
+             max-connections-per-destination 64
+             max-threads 50
+             min-threads 4
+             pending-ops-limit 64}
+     :as   config}]
+   (let [jetty-client (doto (HttpClient. (SslContextFactory$Client.))
+                        (.setAddressResolutionTimeout resolve-timeout)
+                        (.setConnectTimeout connect-timeout)
+                        (.setMaxConnectionsPerDestination max-connections-per-destination)
+                        ;; these are Jetty defaults, except for the queue size
+                        (.setExecutor (doto (org.eclipse.jetty.util.thread.QueuedThreadPool.
+                                             max-threads min-threads 60000
+                                             (java.util.concurrent.LinkedBlockingQueue. ^int (* 2 pending-ops-limit)))
+                                        (.setName "aws-api-jetty")
+                                        (.setDaemon true))))]
+     ;; This is jetty defaults, except daemonizing scheduler
+     (.setScheduler jetty-client (org.eclipse.jetty.util.thread.ScheduledExecutorScheduler.
                                   (str (-> jetty-client class .getSimpleName) "@" (.hashCode jetty-client) "-scheduler") true))
-    (when proxy-port
-      (-> jetty-client
-          .getProxyConfiguration
-          .getProxies
-          (.add (Socks4Proxy.
-                  (or ^String proxy-host
-                      "localhost")
-                  ^int proxy-port))))
-    (.start jetty-client)
-    (Client.
+     (.start jetty-client)
+     (Client.
       jetty-client
       (atom 0)
-      pending-ops-limit)))
+      pending-ops-limit))))
 
-(defn create
-  []
-  ;; FIX :trust-all
-  (create* {:trust-all true}))
+(comment
+  (a/<!! (aws/-submit c {:scheme :https
+                         :server-name "lwn.net"
+                         :server-port 443
+                         :uri "/"
+                         :request-method :get}
+                      (a/chan 1))))
