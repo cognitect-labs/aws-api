2226196bd6d6b5c6596c01ef8d81614f0168afaf
Fogus <mefogus@gmail.com>
2021-11-29 14:31:04 -0500

    Fixes #197 - Added an entry to the status->anomaly table

--- src/cognitect/aws/protocols/common.clj
+++ src/cognitect/aws/protocols/common.clj
@@ -12,6 +12,7 @@
 (def status-codes->anomalies
   {403 :cognitect.anomalies/forbidden
    404 :cognitect.anomalies/not-found
+   429 :cognitect.anomalies/busy
    503 :cognitect.anomalies/busy
    504 :cognitect.anomalies/unavailable})
 
