caa05fab398cbe7f0f0fb43ab83f2b9a6d51a9a3
Thayanne Sousa <thayanne.sousa@nubank.com.br>
2023-02-13 14:01:40 -0300

    add integration test

---
+++ test/src/cognitect/aws/http/java/integration_test.clj
@@ -0,0 +1,55 @@
+(ns cognitect.aws.http.java.integration-test
+  (:require [clojure.test :refer [deftest is testing use-fixtures]]
+            [cognitect.aws.client.api :as aws]
+            [clojure.java.io :as io])
+  (:import (java.time Instant)))
+
+(defn ensure-test-profile
+  [f]
+  (if (= "aws-api-test" (System/getenv "AWS_PROFILE"))
+    (f)
+    (println "AWS_PROFILE is not configured, so not running integration tests. See README.")))
+
+(use-fixtures :once ensure-test-profile)
+
+(defn bucket-listed? [list-buckets-response bucket-name]
+  (contains? (->> list-buckets-response
+                  :Buckets
+                  (map :Name)
+                  (into #{}))
+             bucket-name))
+
+(deftest ^:integration test-s3-client
+  (let [s3 (aws/client {:api :s3})
+        bucket-name (str "aws-api-test-bucket-" (.getEpochSecond (Instant/now)))]
+    
+    (testing ":CreateBucket"
+      (aws/invoke s3 {:op :CreateBucket :request {:Bucket bucket-name}})
+      (is (bucket-listed? (aws/invoke s3 {:op :ListBuckets}) bucket-name)))
+
+    (testing ":PutObject with byte-array"
+      (aws/invoke s3 {:op :PutObject :request {:Bucket bucket-name
+                                               :Key    "hello.txt"
+                                               :Body   (.getBytes "Hello!")}})
+      (is (= "Hello!" (->> (aws/invoke s3 {:op      :GetObject
+                                           :request {:Bucket bucket-name
+                                                     :Key    "hello.txt"}})
+                           :Body
+                           slurp))))
+
+    (testing ":PutObject with input-stream"
+      (aws/invoke s3 {:op :PutObject :request {:Bucket bucket-name
+                                               :Key    "hai.txt"
+                                               :Body   (io/input-stream (.getBytes "Oh hai!"))}})
+      (is (= "Oh hai!" (->> (aws/invoke s3 {:op      :GetObject
+                                            :request {:Bucket bucket-name
+                                                      :Key    "hai.txt"}})
+                            :Body
+                            slurp))))
+    
+    (testing ":DeleteObjects and :DeleteBucket"
+      (aws/invoke s3 {:op :DeleteObjects :request {:Bucket bucket-name
+                                                   :Delete {:Objects [{:Key "hello.txt"}
+                                                                      {:Key "hai.txt"}]}}})
+      (aws/invoke s3 {:op :DeleteBucket :request {:Bucket bucket-name}})
+      (is (not (bucket-listed? (aws/invoke s3 {:op :ListBuckets}) bucket-name))))))
