99f0ee93ec1451efabc169d8766c338de0175d36
Scott Bale <scott.bale@nubank.com.br>
2026-04-07 15:18:14 -0500

    Enhance test fixture to detect if AWS env var secrets are not set

--- test/src/cognitect/aws/integration/fixtures.clj
+++ test/src/cognitect/aws/integration/fixtures.clj
@@ -1,4 +1,5 @@
-(ns cognitect.aws.integration.fixtures)
+(ns cognitect.aws.integration.fixtures
+  (:require [clojure.string :as s]))
 
 (defn ensure-test-profile
   "Ensure AWS_PROFILE env var is aws-api-test
@@ -8,5 +9,7 @@
   (use-fixtures :once aux.integration/ensure-test-profile)"
   [f]
   (if (= "aws-api-test" (System/getenv "AWS_PROFILE"))
-    (f)
+    (if (s/blank? (System/getenv "AWS_ACCESS_KEY_ID"))
+      (println "AWS_* secret env vars not available, so not running integration tests.")
+      (f))
     (println "AWS_PROFILE is not configured, so not running integration tests.")))
