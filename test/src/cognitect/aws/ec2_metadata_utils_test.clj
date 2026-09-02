360107f0bf1773fa3fb2c1ebf6dee8d6b132dfce
Marco Biscaro <marco.biscaro@nubank.com.br>
2026-09-02 12:12:44 -0300

    Add support for AWS_CONTAINER_AUTHORIZATION_TOKEN_FILE
    Fixes https://github.com/cognitect-labs/aws-api/issues/286

--- test/src/cognitect/aws/ec2_metadata_utils_test.clj
+++ test/src/cognitect/aws/ec2_metadata_utils_test.clj
@@ -7,8 +7,8 @@
             [cognitect.aws.ec2-metadata-utils :as ec2-metadata-utils]
             [cognitect.aws.http :as http]
             [cognitect.aws.util :as u])
-  (:import [java.net URI]))
-
+  (:import [java.io File]
+           [java.net URI]))
 
 (deftest returns-nil-after-retries
   ;; Using a mock http client that keeps track of the number of http requests and always returns a
@@ -39,3 +39,34 @@
     (is (nil? (get-in (#'ec2-metadata-utils/request-map (URI/create "http://localhost")) [:headers "Authorization"])))
     (with-redefs [u/getenv {"AWS_CONTAINER_AUTHORIZATION_TOKEN" "this-is-the-token"}]
       (is (#{"this-is-the-token"} (get-in (#'ec2-metadata-utils/request-map (URI/create "http://localhost")) [:headers "Authorization"]))))))
+
+(deftest auth-token-file
+  ;; https://docs.aws.amazon.com/eks/latest/userguide/pod-id-how-it-works.html#pod-id-agent-pod
+  (let [token-file  (File/createTempFile "eks-pod-identity-token" "")
+        auth-header #(get-in (#'ec2-metadata-utils/request-map (URI/create "http://169.254.170.23/v1/credentials"))
+                             [:headers "Authorization"])]
+    (try
+      (spit token-file "eks-token-value\n")
+
+      (testing "the token is read from the file named by AWS_CONTAINER_AUTHORIZATION_TOKEN_FILE"
+        (with-redefs [u/getenv {"AWS_CONTAINER_AUTHORIZATION_TOKEN_FILE" (.getPath token-file)}]
+          (is (= "eks-token-value" (auth-header)))))
+
+      (testing "the file is re-read per request, so a rotated token is picked up"
+        (with-redefs [u/getenv {"AWS_CONTAINER_AUTHORIZATION_TOKEN_FILE" (.getPath token-file)}]
+          (is (= "eks-token-value" (auth-header)))
+          (spit token-file "rotated-token\n")
+          (is (= "rotated-token" (auth-header)))))
+
+      (testing "AWS_CONTAINER_AUTHORIZATION_TOKEN wins when both are set"
+        ;; matches ContainerCredentialsProvider#getTokenValue in aws-sdk-java-v2
+        (with-redefs [u/getenv {"AWS_CONTAINER_AUTHORIZATION_TOKEN"      "this-is-the-token"
+                                "AWS_CONTAINER_AUTHORIZATION_TOKEN_FILE" (.getPath token-file)}]
+          (is (= "this-is-the-token" (auth-header)))))
+
+      (testing "an unreadable token file yields no Authorization header, and does not throw"
+        (with-redefs [u/getenv {"AWS_CONTAINER_AUTHORIZATION_TOKEN_FILE" (str (.getPath token-file) ".does-not-exist")}]
+          (is (nil? (auth-header)))))
+
+      (finally
+        (.delete token-file)))))
